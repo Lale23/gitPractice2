896b2d63d89ecf8232c690f0ca41fb201fdb7de2
Lale23 <62312768+Lale23@users.noreply.github.com>
2020-04-24 16:29:44 -0400

    Update GitFirstDay.java

--- src/day_1/GitFirstDay.java
+++ src/day_1/GitFirstDay.java
@@ -3,5 +3,8 @@ package day_1;
 public class GitFirstDay {
     public static void main(String[] args) {
         System.out.println("Spending 3 hours to learn git things");
+   
+       //mdkcncksnksncksdjdjkdndjisdjo
+     
     }
 }
